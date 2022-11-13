45f7bd66c8d9ea5a30a50b6a2566a9d0f9a69867
park-siwan <siwandevelop@gmail.com>
2022-11-13 16:51:29 +0900

    데코레이터(Decorator) 학습, query parameter의 @Query() body decorators의 @Body()

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -6,15 +15,24 @@ export class MoviesController {
   getAll() {
     return 'This will return all movies';
   }
+  /**
+   * search 부분이 get보다 밑에 있으면 NestJS는 search를 id로 판단한다.
+   * search === :id
+   */
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are search for a movie with a title${searchingYear} `;
+  }
 
-  @Get('/:id') //Route handler (method) Decorator
+  @Get(':id') //Route handler (method) Decorator
   //ParameterDecorator
   getOne(@Param('id') movieId: string) {
     return `This will return one movie with the id: ${movieId}`;
   }
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData) {
+    console.log(movieData);
+    return movieData;
   }
 
   @Delete()
@@ -22,8 +40,11 @@ export class MoviesController {
     return `This will delete a movie with the id: ${movieId}`;
   }
 
-  @Patch('/:id')
-  patch(@Param('id') movieId: string) {
-    return `This will patch a movie with the id ${movieId}`;
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updateData: movieId,
+      ...updateData,
+    };
   }
 }
