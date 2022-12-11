c94d6dada1fe791031d8c10fdc72fe5186c0124d
park-siwan <siwandevelop@gmail.com>
2022-12-11 19:09:06 +0900

    feat: [#2.3] 컨트롤러와 서비스 개념 숙지, insomnia로 get post 테스트

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,12 +8,16 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return 'This will return all movies';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
   /**
    * search 부분이 get보다 밑에 있으면 NestJS는 search를 id로 판단한다.
@@ -26,18 +30,20 @@ export class MoviesController {
 
   @Get(':id') //Route handler (method) Decorator
   //ParameterDecorator
-  getOne(@Param('id') movieId: string) {
-    return `This will return one movie with the id: ${movieId}`;
+  getOne(@Param('id') movieId: string): Movie {
+    // return `This will return one movie with the id: ${movieId}`;
+    return this.moviesService.getOne(movieId);
   }
   @Post()
   create(@Body() movieData) {
-    console.log(movieData);
-    return movieData;
+    // return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete()
   remove(@Param('id') movieId: string) {
-    return `This will delete a movie with the id: ${movieId}`;
+    // return `This will delete a movie with the id: ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+  /**
+   * parseInt(id) 와 +id 가 같은 결과임
+   * string을 number로 바꿀 수 있다.
+   */
+  getOne(id: string): Movie {
+    return this.movies.find((movie) => movie.id === +id);
+  }
+
+  deleteOne(id: string): boolean {
+    this.movies.filter((movie) => movie.id !== +id);
+    return true;
+  }
+  create(movieData) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+  }
+}
