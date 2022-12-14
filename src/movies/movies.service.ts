a1270807cd38fbf5a5b5108865d16cefd00c7b04
park-siwan <siwandevelop@gmail.com>
2022-12-14 23:50:03 +0900

    feat: get시 not found, 삭제기능

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -13,13 +13,18 @@ export class MoviesService {
    * string을 number로 바꿀 수 있다.
    */
   getOne(id: string): Movie {
-    return this.movies.find((movie) => movie.id === +id);
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID: ${id} not found`);
+    }
+    return movie;
   }
 
-  deleteOne(id: string): boolean {
-    this.movies.filter((movie) => movie.id !== +id);
-    return true;
+  deleteOne(id: string) {
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
   }
+
   create(movieData) {
     this.movies.push({
       id: this.movies.length + 1,
